f773d9ed8e9b67c9ea6dd5c72354b6eb7d7ac15b
SchaefferGeorgiou <schaeffergeorgiou@gmail.com>
2022-03-03 15:46:11 +0000

    "Fixed" multiple lights and completed BlinnPhong
    Found spelling error in the location change of the lights which was the reason it wasn't adding properly.
    
    Found out how to deep fry and cause seizure by changing how lights are added.
    
    Started working on Spotlights.

--- Project_Template/scenebasic_uniform.h
+++ Project_Template/scenebasic_uniform.h
@@ -6,6 +6,7 @@
 #include <glad/glad.h>
 #include "helper/glslprogram.h"
 #include "helper/torus.h"
+#include "helper/teapot.h"
 
 #include "helper/plane.h"
 #include "helper/objmesh.h"
@@ -14,7 +15,8 @@ class SceneBasic_Uniform : public Scene
 {
 private:
     GLSLProgram prog;
-    //Torus torus;
+    Torus torus;
+    Teapot teapot;
     Plane plane;
     std::unique_ptr<ObjMesh> mesh;
 
